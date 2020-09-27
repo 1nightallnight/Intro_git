65e824e9ccab0188e2e8516554ad450a8d0974c9
Karen Abundis <karen.abundis3673@alumnos.udg.mx>
2020-09-27 18:15:07 -0500

    Comentamos el hola mundo

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){	
+int main(){
 	char nombre[100];
 	unsigned int edad;
-	
-	printf("Hola mundo\n");
+
+	//printf("Hola mundo\n");
     printf("Nombre: ");
 	fgets(nombre, sizeof(nombre),stdin);
-	printf("Hola %s",nombre);	
+	printf("Hola %s",nombre);
 	printf("Edad: ");
 	scanf("%u",&edad);
 	printf("Vas a cumplir %u", edad+1);
